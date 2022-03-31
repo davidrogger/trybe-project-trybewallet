c0f30d86a418a98d93a5a1ed5d1d1f0470653943
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-31 16:57:58 -0300

    adicionada rota para a pagina wallet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
+import Wallet from './pages/Wallet';
 
 function App() {
   return (
     <main className="main-container">
       <Switch>
         <Route exact path="/" component={ Login } />
+        <Route exact path="/carteira" component={ Wallet } />
       </Switch>
     </main>
   );
