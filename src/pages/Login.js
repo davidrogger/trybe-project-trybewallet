ea66d85302d3acdf3477f74f8066ea2018a4caa1
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-31 16:24:50 -0300

    criacao do compoponente login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import '../styles/login.css';
 
+import Input from '../components/Input';
+
 class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
+  }
+
+  loginHandler = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { email, password } = this.state;
     return (
       <div className="login-container">
-        login
+
+        <h1>TrybeWallet</h1>
+
+        <div>
+          <Input
+            data-testid="email-input"
+            name="email"
+            type="Text"
+            placeholder="Email"
+            onChange={ this.loginHandler }
+            value={ email }
+          />
+
+          <Input
+            data-testid="password-input"
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={ this.loginHandler }
+            value={ password }
+          />
+
+          <div className="d-grid col-6 mx-auto">
+            <button
+              type="button"
+              className="btn btn-warning btn-lg"
+              // disabled
+            >
+              Entrar
+
+            </button>
+
+          </div>
+        </div>
       </div>
     );
   }
