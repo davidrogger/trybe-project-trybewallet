747a438ebbe6429fb972a7e75e897d8e4a2670d2
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-31 16:55:46 -0300

    criacao da action que coleta o e-mail do login para store

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,5 @@
-// Coloque aqui suas actions
+import { GET_USER_EMAIL } from '../reducers/user';
+
+const getUserEmail = (email) => ({ type: GET_USER_EMAIL, email });
+
+export default getUserEmail;
