4573d4851994aace133a64de9958a22c9aa41d33
Davíd Roggér <davidrogger.a@gmail.com>
2022-03-31 16:25:19 -0300

    criacao do componente input para padrao de estilo

---
+++ src/components/Input.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Input extends Component {
+  render() {
+    const { name, type, value, placeholder, onChange } = this.props;
+    return (
+      <div className="form-floating mb-3">
+        <input
+          name={ name }
+          id={ `input-${name}` }
+          className="input-user form-control form__input"
+          type={ type }
+          placeholder={ placeholder }
+          onChange={ onChange }
+          value={ value }
+        />
+        <label htmlFor={ `input-${name}` }>{ placeholder }</label>
+      </div>
+    );
+  }
+}
+
+Input.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  value: PropTypes.string,
+}.isRequired;
+
+export default Input;
