a9842db75dafc449452d2b4ad3439d4c60ced3d0
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-01 13:43:17 -0300

    criada action para salvar informacoes no estado expenses

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 export const GET_USER_EMAIL = 'GET_USER_EMAIL';
 export const GET_CURRENCIES = 'GET_CURRENCIES';
+export const ADD_EXPENSE = 'ADD_EXPENSE';
 
 export const getUserEmail = (email) => ({ type: GET_USER_EMAIL, email });
 
 export const getCurrencies = (currencies) => ({ type: GET_CURRENCIES, currencies });
+
+export const addExpense = (expenses) => ({ type: ADD_EXPENSE, expenses });
 
 export const fetchAPI = () => async (dispatch) => {
   try {
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,4 +1,4 @@
-import { GET_CURRENCIES } from '../actions';
+import { GET_CURRENCIES, ADD_EXPENSE } from '../actions';
 
 const INITIAL_STATE = {
   currencies: [],
@@ -11,6 +11,11 @@ export const wallet = (state = INITIAL_STATE, action) => {
     return {
       ...state,
       currencies: action.currencies,
+    };
+  case ADD_EXPENSE:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.expense],
     };
   default:
     return state;
