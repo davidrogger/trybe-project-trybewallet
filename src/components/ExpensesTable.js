e008e195faa0accfcda85b70db5592271bea4914
Davíd Roggér <davidrogger.a@gmail.com>
2022-04-01 18:53:44 -0300

    ajustes de estilo para o botao de alinhamento das colunas da tabela

--- src/components/ExpensesTable.js
+++ src/components/ExpensesTable.js
@@ -45,14 +45,14 @@ class ExpensesTable extends Component {
 
   render() {
     return (
-      <table className="tabel-container table table-dark table-sm table-striped">
+      <table className="table-container table table-dark table-sm table-striped">
         <thead>
           <tr>
-            <th>Descrição</th>
+            <th className="description-column">Descrição</th>
             <th>Tag</th>
             <th>Método de pagamento</th>
             <th>Valor</th>
-            <th>Moeda</th>
+            <th className="coin-column">Moeda</th>
             <th>Câmbio utilizado</th>
             <th>Valor convertido</th>
             <th>Moeda de conversão</th>
