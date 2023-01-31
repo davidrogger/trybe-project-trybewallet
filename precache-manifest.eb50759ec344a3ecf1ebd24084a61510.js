74c7e7679f0dd3328e7e34c1005e85725bfb3876
Davíd Roggér <davidrogger.a@gmail.com>
2023-01-31 12:06:18 -0300

    Updates

---
+++ precache-manifest.eb50759ec344a3ecf1ebd24084a61510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b8797710424a3ae0691555333d19a9",
+    "url": "/trybe-project-trybewallet/index.html"
+  },
+  {
+    "revision": "001c91d5dd8d222c0f10",
+    "url": "/trybe-project-trybewallet/static/css/main.b14f975e.chunk.css"
+  },
+  {
+    "revision": "124bdda55377c77ab26c",
+    "url": "/trybe-project-trybewallet/static/js/2.c4370cc0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybe-project-trybewallet/static/js/2.c4370cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "001c91d5dd8d222c0f10",
+    "url": "/trybe-project-trybewallet/static/js/main.42ba82bb.chunk.js"
+  },
+  {
+    "revision": "dbdfce294d603dfd8fb9",
+    "url": "/trybe-project-trybewallet/static/js/runtime-main.ad2de22f.js"
+  }
+]);
